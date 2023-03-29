c1fdcb485fa2c4eda9c600c3824e66e0ac47c686
Jiakun Zhao <jiakun.zhao@outlook.com>
2023-03-29 12:51:27 +0800

    chore: update dependencies version

--- index.cjs
+++ index.cjs
@@ -3,11 +3,12 @@ module.exports = {
     rules: {
         // 其实就是对 antfu 配置的一些覆盖
 
-        indent: ['warn', 4],
+        'indent': ['warn', 4],
         '@typescript-eslint/indent': ['warn', 4],
         'jsonc/indent': ['warn', 4],
         'vue/html-indent': ['warn', 4],
         'yml/indent': ['warn', 4],
+        'jsonc/indent': ['warn', 4],
 
         'brace-style': ['warn', '1tbs'],
         '@typescript-eslint/brace-style': ['warn', '1tbs'],
