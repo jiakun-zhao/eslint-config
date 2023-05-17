7fb42ec94c8112d99b34aa39ad4386a1df8d3b7a
Jiakun Zhao <50269993+jiakun-zhao@users.noreply.github.com>
2023-05-17 14:26:44 +0800

    chore: remove tab size set (before 4 after 2)

--- index.cjs
+++ index.cjs
@@ -1,22 +1,22 @@
 module.exports = {
-    extends: ['@antfu'],
-    rules: {
-        // 其实就是对 antfu 配置的一些覆盖
+  extends: ["@antfu"],
+  rules: {
+    // 其实就是对 antfu 配置的一些覆盖
 
-        'indent': ['warn', 4],
-        '@typescript-eslint/indent': ['warn', 4],
-        'jsonc/indent': ['warn', 4],
-        'vue/html-indent': ['warn', 4],
-        'yml/indent': ['warn', 4],
-        'jsonc/indent': ['warn', 4],
+    // 'indent': ['warn', 4],
+    // '@typescript-eslint/indent': ['warn', 4],
+    // 'jsonc/indent': ['warn', 4],
+    // 'vue/html-indent': ['warn', 4],
+    // 'yml/indent': ['warn', 4],
+    // 'jsonc/indent': ['warn', 4],
 
-        'brace-style': ['warn', '1tbs'],
-        '@typescript-eslint/brace-style': ['warn', '1tbs'],
+    "brace-style": ["warn", "1tbs"],
+    "@typescript-eslint/brace-style": ["warn", "1tbs"],
 
-        'vue/singleline-html-element-content-newline': 'off',
-        'vue/html-self-closing': 'off',
-        'vue/static-class-names-order': 'warn',
+    "vue/singleline-html-element-content-newline": "off",
+    "vue/html-self-closing": "off",
+    "vue/static-class-names-order": "warn",
 
-        'no-console': 'warn',
-    },
-}
+    "no-console": "warn",
+  },
+};
