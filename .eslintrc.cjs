464acec11a01b212807dcad5f28138db8f6cc4b9
Jiakun Zhao <50269993+jiakun-zhao@users.noreply.github.com>
2023-06-02 16:11:56 +0800

    feat: add react

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,8 @@
 module.exports = {
-  extends: '@antfu',
+  extends: [
+    '@antfu',
+    '@antfu/eslint-config-react',
+  ],
   rules: {
     // https://eslint.org
     // https://typescript-eslint.io
@@ -19,6 +22,26 @@ module.exports = {
         'jsonc/sort-keys': 'off',
         'jsonc/key-name-casing': 'off',
         'jsonc/array-bracket-newline': ['error', 'consistent'],
+      },
+    },
+    {
+      // react: https://github.com/jsx-eslint/eslint-plugin-react/tree/master#list-of-supported-rules
+      files: ['*.jsx', '*.tsx'],
+      rules: {
+        'react/jsx-wrap-multilines': ['error', { declaration: 'parens-new-line', assignment: 'parens-new-line', return: 'parens-new-line', arrow: 'parens-new-line', condition: 'parens-new-line', logical: 'parens-new-line', prop: 'parens-new-line' }],
+        'react/jsx-tag-spacing': ['error', { closingSlash: 'never', beforeClosing: 'never' }],
+        'react/jsx-pascal-case': ['warn', { allowAllCaps: true }],
+        'react/jsx-one-expression-per-line': ['warn', { allow: 'single-child' }],
+        'react/jsx-indent-props': ['error', 2],
+        'react/jsx-indent': ['error', 2, { checkAttributes: true, indentLogicalExpressions: true }],
+        'react/jsx-equals-spacing': ['error', 'never'],
+        'react/jsx-curly-newline': ['error', { multiline: 'consistent', singleline: 'consistent' }],
+        'react/jsx-curly-brace-presence': ['error', { props: 'never', children: 'never', propElementValues: 'always' }],
+        'react/jsx-closing-tag-location': 'error',
+        'react/jsx-closing-bracket-location': 'error',
+        'react/jsx-boolean-value': ['error', 'never'],
+        'react/destructuring-assignment': ['error', 'never'],
+        'react/no-unknown-property': 'off',
       },
     },
   ],
