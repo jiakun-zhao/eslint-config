8650d59e02a828b816f0e6d3f730ec264d376654
Jiakun Zhao <50269993+jiakun-zhao@users.noreply.github.com>
2024-03-11 12:20:14 +0800

    chore: update

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,20 @@
-import type { ESLint } from 'eslint'
+import antfu from '@antfu/eslint-config'
 
-export default <ESLint.ConfigData>{
-  extends: [
-    '@antfu',
-    'plugin:react/recommended',
-  ],
-  rules: {
-    'no-console': 'warn',
-    'brace-style': ['warn', '1tbs'],
-    '@typescript-eslint/brace-style': ['warn', '1tbs'],
-    '@typescript-eslint/ban-ts-comment': ['off'],
-
-    '@stylistic/ts/brace-style': 'off',
-
-    // imports
-    'unused-imports/no-unused-imports': 'warn',
-
-    // vue
-    'vue/singleline-html-element-content-newline': 'off',
-    'vue/html-self-closing': 'off',
-    'vue/static-class-names-order': 'warn',
-
-    // react & jsx
-    'jsx-quotes': ['error', 'prefer-single'],
-
-    'react/react-in-jsx-scope': 'off',
-    'react/no-unknown-property': 'off',
-    'react/self-closing-comp': 'off',
-    'react/destructuring-assignment': 'off',
-    'react/no-multi-comp': 'off',
-    'react/function-component-definition': 'off',
-    'react/display-name': 'off',
-    'react/button-has-type': 'off',
-    'react/no-children-prop': 'off',
-    'react/jsx-key': 'off',
-    'react/jsx-indent': ['error', 2],
-    'react/jsx-indent-props': ['error', 2],
-    'react/jsx-max-depth': 'off',
-    'react/jsx-filename-extension': 'off',
-    'react/jsx-no-literals': 'off',
-    'react/jsx-max-props-per-line': 'off',
-    'react/jsx-tag-spacing': ['error', { closingSlash: 'never', beforeSelfClosing: 'always', afterOpening: 'never', beforeClosing: 'never' }],
-    'react/jsx-newline': 'off',
-    'react/jsx-one-expression-per-line': 'off',
-    'react/jsx-no-bind': 'off',
-    'react/jsx-curly-spacing': ['error', { when: 'never', children: { when: 'never' } }],
-    'react/jsx-closing-bracket-location': 'warn',
+export default antfu({
+  stylistic: {
+    overrides: {
+      'style/jsx-one-expression-per-line': 'off',
+    },
   },
-}
+  typescript: {
+    overrides: {
+      'ts/ban-ts-comment': 'off',
+    },
+  },
+  vue: {
+    overrides: {
+      'vue/html-self-closing': 'off',
+      'vue/singleline-html-element-content-newline': 'off',
+    },
+  },
+})
