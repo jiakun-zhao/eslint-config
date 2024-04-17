637032401d7222fb340d7d092c519b1c0421d7ba
Jiakun Zhao <50269993+jiakun-zhao@users.noreply.github.com>
2024-04-17 08:42:30 +0800

    chore: update

--- build.config.ts
+++ build.config.ts
@@ -1,10 +1,8 @@
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
-  entries: ['src/index'],
+  entries: [
+    'src/index',
+  ],
   clean: true,
-  declaration: false,
-  rollup: {
-    emitCJS: false,
-  },
 })
--- src/index.ts
+++ src/index.ts
@@ -1,74 +1,48 @@
 import antfu from '@antfu/eslint-config'
+import { isPackageExists } from 'local-pkg'
 
 export default antfu({
-  rules: {
-    'no-console': 'warn',
-  },
   stylistic: {
     overrides: {
-      'style/jsx-one-expression-per-line': 'off',
-    },
-  },
-  typescript: {
-    overrides: {
+      'no-console': 'warn',
+      // typescript
       'ts/ban-ts-comment': 'off',
+      // unused-imports
+      'unused-imports/no-unused-imports': 'warn',
+      // jsx
+      'style/jsx-quotes': ['off', 'prefer-single'],
+      'style/jsx-one-expression-per-line': 'off',
     },
   },
   vue: {
     overrides: {
       'vue/html-self-closing': 'off',
       'vue/singleline-html-element-content-newline': 'off',
+      'vue/static-class-names-order': 'warn',
     },
   },
+  astro: isPackageExists('eslint-plugin-astro'),
 })
 
-// import type { ESLint } from 'eslint'
-
-// export default <ESLint.ConfigData>{
-//   extends: [
-//     '@antfu',
-//     'plugin:react/recommended',
-//   ],
-//   rules: {
-//     'no-console': 'warn',
-//     'brace-style': ['warn', '1tbs'],
-//     '@typescript-eslint/brace-style': ['warn', '1tbs'],
-//     '@typescript-eslint/ban-ts-comment': ['off'],
-
-//     '@stylistic/ts/brace-style': 'off',
-
-//     // imports
-//     'unused-imports/no-unused-imports': 'warn',
-
-//     // vue
-//     'vue/singleline-html-element-content-newline': 'off',
-//     'vue/html-self-closing': 'off',
-//     'vue/static-class-names-order': 'warn',
-
-//     // react & jsx
-//     'jsx-quotes': ['error', 'prefer-single'],
-
-//     'react/react-in-jsx-scope': 'off',
-//     'react/no-unknown-property': 'off',
-//     'react/self-closing-comp': 'off',
-//     'react/destructuring-assignment': 'off',
-//     'react/no-multi-comp': 'off',
-//     'react/function-component-definition': 'off',
-//     'react/display-name': 'off',
-//     'react/button-has-type': 'off',
-//     'react/no-children-prop': 'off',
-//     'react/jsx-key': 'off',
-//     'react/jsx-indent': ['error', 2],
-//     'react/jsx-indent-props': ['error', 2],
-//     'react/jsx-max-depth': 'off',
-//     'react/jsx-filename-extension': 'off',
-//     'react/jsx-no-literals': 'off',
-//     'react/jsx-max-props-per-line': 'off',
-//     'react/jsx-tag-spacing': ['error', { closingSlash: 'never', beforeSelfClosing: 'always', afterOpening: 'never', beforeClosing: 'never' }],
-//     'react/jsx-newline': 'off',
-//     'react/jsx-one-expression-per-line': 'off',
-//     'react/jsx-no-bind': 'off',
-//     'react/jsx-curly-spacing': ['error', { when: 'never', children: { when: 'never' } }],
-//     'react/jsx-closing-bracket-location': 'warn',
-//   },
-// }
+// 'react/react-in-jsx-scope': 'off',
+// 'react/no-unknown-property': 'off',
+// 'react/self-closing-comp': 'off',
+// 'react/destructuring-assignment': 'off',
+// 'react/no-multi-comp': 'off',
+// 'react/function-component-definition': 'off',
+// 'react/display-name': 'off',
+// 'react/button-has-type': 'off',
+// 'react/no-children-prop': 'off',
+// 'react/jsx-key': 'off',
+// 'react/jsx-indent': ['error', 2],
+// 'react/jsx-indent-props': ['error', 2],
+// 'react/jsx-max-depth': 'off',
+// 'react/jsx-filename-extension': 'off',
+// 'react/jsx-no-literals': 'off',
+// 'react/jsx-max-props-per-line': 'off',
+// 'react/jsx-tag-spacing': ['error', { closingSlash: 'never', beforeSelfClosing: 'always', afterOpening: 'never', beforeClosing: 'never' }],
+// 'react/jsx-newline': 'off',
+// 'react/jsx-one-expression-per-line': 'off',
+// 'react/jsx-no-bind': 'off',
+// 'react/jsx-curly-spacing': ['error', { when: 'never', children: { when: 'never' } }],
+// 'react/jsx-closing-bracket-location': 'warn',
